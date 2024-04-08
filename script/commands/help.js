9271989219b0dd0b188d57900e142ff77a44d63b
Harold Hutchins <160975590+haroldhutchin2005@users.noreply.github.com>
2024-04-09 00:10:46 +0800

    Update help.js

--- script/commands/help.js
+++ script/commands/help.js
@@ -116,7 +116,7 @@ module.exports.run = async function ({ api, event, args }) {
 
   const randomQuote = randomQuotes[Math.floor(Math.random() * randomQuotes.length)];
 
-  msg += `├─────☾⋆\n│ » Total commands: [ ${commands.size} ]\n│「 ☾⋆ PREFIX: ${global.config.PREFIX} 」\n╰──────────⧕\n\n𝗥𝗔𝗡𝗗𝗢𝗠 𝗙𝗔𝗖𝗧: ${randomQuote}\n\nOwner: ${global.config.BOTOWNER}`;
+  msg += `├─────☾⋆\n│ » Total commands: [ ${commands.size} ]\n│  PREFIX: ${global.config.PREFIX} `;
 
 
   return api.sendMessage(msg, threadID, async (error, info) => {
