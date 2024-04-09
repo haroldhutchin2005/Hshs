23bbba85226407ad6b970f912b42a26c0aa64318
Harold Hutchins <160975590+haroldhutchin2005@users.noreply.github.com>
2024-04-09 15:23:39 +0800

    Create songs.js

---
+++ script/commands/songs.js
@@ -0,0 +1,42 @@
+const axios = require('axios');
+
+module.exports.config = {
+    name: "songs",
+    version: "1.0.0",
+    hasPermssion: 0,
+    credits: "Jonell Magallanes",
+    description: "Search for songs from GDPH SERVER",
+    usePrefix: true,
+    commandCategory: "GDPH TOOLS",
+    usages: "[song title]",
+    cooldowns: 30
+};
+
+module.exports.run = async function ({ api, event, args }) {
+    const title = encodeURIComponent(args.join(" "));
+    const apiUrl = `https://gpdh-server-song-list-api-search-by.onrender.com/gdph?songlist=${title}`;
+
+    if (!title) return api.sendMessage("Please provide a song title.\n\nExample: songsearch TIDAL WAVE", event.threadID, event.messageID);
+
+    try {
+        const searchMessage = await api.sendMessage("🔍 | Checking The Database for Searching songs. Please wait...", event.threadID);
+
+        const response = await axios.get(apiUrl);
+        const songs = response.data;
+
+        if (songs.length === 0) {
+            await api.sendMessage("No songs found with that title.", event.threadID);
+            return api.unsendMessage(searchMessage.messageID);
+        }
+
+        for (const song of songs) {
+            let resultMessage = `ID: ${song.id}\nSong: ${song.song}\nAuthor: ${song.author}\nSize: ${song.size}\n\n`;
+            await api.sendMessage(resultMessage, event.threadID);
+        }
+
+        api.unsendMessage(searchMessage.messageID);
+    } catch (error) {
+        console.error(error);
+        api.sendMessage("An error occurred while processing your request.", event.threadID);
+    }
+};
