a78df4be80538554a4088b60fbf0524d7e00c44d
Harold Hutchins <160975590+haroldhutchin2005@users.noreply.github.com>
2024-04-09 09:37:13 +0800

    Create gdphtop.js

---
+++ script/commands/gdphtop.js
@@ -0,0 +1,42 @@
+const axios = require('axios');
+
+module.exports.config = {
+    name: "gdphtop",
+    version: "1.0.0",
+    hasPermssion: 0,
+    credits: "Jonell Magallanes",
+    description: "GDPH leaderboard",
+    usePrefix: false,
+    commandCategory: "GDPH TOOLS",
+    cooldowns: 30
+};
+
+module.exports.run = async function ({ api, event }) {
+    const apiUrl = 'https://gdphtopleaderboare-d70496a9c158.herokuapp.com/gdphtop';
+
+    try {
+        const response = await axios.get(apiUrl);
+        const leaderboard = response.data.slice(0, 27);
+
+        let leaderboardMessage = "📊 Top 27 Leaderboard On GDPH Server:\n\n";
+        
+        leaderboard.forEach((entry, index) => {
+            leaderboardMessage += `Rank ${entry.rank}: ${entry.username} (UserID: ${entry.userID}) - Stars: ${entry.stars}\n\n`;
+        });
+
+        api.sendMessage(leaderboardMessage, event.threadID, async (error, messageInfo) => {
+            if (error) {
+                console.error(error);
+                api.sendMessage("An error occurred while sending the leaderboard.", event.threadID);
+                return;
+            }
+
+            setTimeout(async () => {
+                await api.unsendMessage(messageInfo.messageID, event.threadID);
+            }, 60000);
+        });
+    } catch (error) {
+        console.error(error);
+        api.sendMessage("An error occurred while fetching the leaderboard.", event.threadID);
+    }
+};
