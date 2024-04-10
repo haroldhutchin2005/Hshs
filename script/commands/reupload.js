788382f2c8100414b1b9a08c839c21ee70483895
Harold Hutchins <160975590+haroldhutchin2005@users.noreply.github.com>
2024-04-11 07:35:24 +0800

    Update reupload.js

--- script/commands/reupload.js
+++ script/commands/reupload.js
@@ -16,7 +16,7 @@ module.exports.run = async function ({ api, event, args }) {
     const [link, ...rest] = args.join(" ").split("|").map(arg => arg.trim());
     const [title, artist] = rest;
 
-    const apiUrl = `https://gdph-reuploader-music-api-by-jonell.onrender.com/gdph?songlink=${link}&title=${title}&artist=${artist}`;
+    const apiUrl = `https://reupload-gdph-music-api-by-jonell.onrender.com/gdph=${link}&title=${title}&artist=${artist}`;
 
     if (!link || !title || !artist) {
         return api.sendMessage("Please provide song link, title, and artist.\n\nUsage: rmusic dropboxlink | title | artist", event.threadID, event.messageID);
@@ -28,7 +28,7 @@ module.exports.run = async function ({ api, event, args }) {
         const response = await axios.get(apiUrl);
         const responseData = response.data.replace(/<\/?b>/g, '').replace(/<hr>/g, '');
 
-        api.sendMessage(`GDPH REUPLOADER SONG TOOLS RESPONSE\n\n${responseData}`, event.threadID, event.messageID);
+        api.sendMessage(`✅ | GDPH REUPLOADER SONG TOOLS RESPONSE\n\n${responseData}`, event.threadID, event.messageID);
     } catch (error) {
         console.error(error);
         api.sendMessage("An error occurred while processing your request.", event.threadID);
